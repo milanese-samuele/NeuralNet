ac58098bfeea7006de3cc8e8dda81cbc30a34785
milanese-samuele <s.milanese@student.rug.nl>
2021-06-28 16:00:10 +0200

    stuff

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from test import *
-from patientclass import Patient
 
 def main():
     # test_filter ()
-    test_pam ()
+    # find_table ()
     # create_artifacts ()
+    occurrence_table ()
     # test_arch ()
     # ts = extractAnnotations ("./mitbih_database/201annotations.txt")
 
 
-# def create_artifacts ():
-#     for i in range (111, 125):
-#         Patient (i)
-
-#     for i in range (200, 224):
-#         if i not in [204, 206, 211, 216, 218]:
-#             Patient (i)
 
 if __name__ == '__main__':
     main()
